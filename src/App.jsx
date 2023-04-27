ba974d56b0d292f5777e5df565e37d0f65460f9e
cremirdevio <josephajibodu@gmail.com>
2023-04-27 18:48:44 +0100

    header and body for todo app built

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-red-200 h-screen flex justify-center items-center">
+      <div className="border-red-100 h-max bg-white w-full max-w-[400px]">
+        <h1 className="text-center my-2 uppercase font-bold text-gray-600">
+          Things To Do
+        </h1>
+
+        <div className="px-4 py-4">
+          {/* Input */}
+          <input
+            type="text"
+            placeholder="Add New Item"
+            className="rounded-none border-[1px] border-solid w-full px-3 py-2 text-xs focus:rounded-none focus:outline-none focus:border-blue-300"
+          />
+
+          {/* Todo List with checkbox */}
+          <div className="flex  gap-2 py-2 border-b-[0.5px]">
+            <input type="checkbox" />
+            <p className="text-gray-600 text-sm">Learn Javascript</p>
+          </div>
+
+          <div className="flex  gap-2 py-2 border-b-[0.5px]">
+            <input type="checkbox" />
+            <p className="text-gray-600 text-sm">Learn React</p>
+          </div>
+
+          <div className="flex  gap-2 py-2 border-b-[0.5px]">
+            <input type="checkbox" />
+            <p className="text-gray-600 text-sm">Learn NodeJs</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between bg-green-200 text-sm py-2 px-2">
+          {/* Footer:  */}
+
+          {/* Search Icon | Number of items left | Filter(All, Active, Completed) */}
+          <div className="flex gap-2">
+            <div>
+              <button>All</button>
+            </div>
+            <p>3 Items left</p>
+          </div>
+
+          <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
